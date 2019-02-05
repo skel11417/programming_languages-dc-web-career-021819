e4499278ee7100173c512251ec273aaaf1ded2aa
Sean Kelly <shonkelli@gmail.com>
2019-02-05 21:00:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,8 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, langs|
     langs.each do | lang_name, type_hash |
-      binding.pry
       new_hash[lang_name] = {}
+      binding.pry
       new_hash[lang_name][:type] = type_hash[:type]
       
       # if new_hash[lang_name][:style].exists?
