8c9d6d26008af930144df0111cc6a9f817146a7b
Sean Kelly <shonkelli@gmail.com>
2019-02-05 21:03:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,10 +8,9 @@ def reformat_languages(languages)
       new_hash[lang_name][:type] = type_hash[:type]
       binding.pry      
       if new_hash[lang_name][:style].nil?
-          new_hash[lang_name][:style] = [style]
+        new_hash[lang_name][:style] = [style]
       else
-          new_hash[lang_name][:style] << style
-        
+        new_hash[lang_name][:style] << style
       end
     end
   end
